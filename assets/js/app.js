3da7895a133afe15706db35a79a72424e210b6b8
bugzweiser <bugzweiser@gmail.com>
2019-05-21 09:45:00 -0700

    start of project

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    //global variables
+    var triva = [{
+            question: "What TV Show features a Dodge Challenger",
+            choice: [
+                "Fall Guy",
+                "Dukes of Hazzard",
+                "CHiPs",
+                "Miami Vice"
+            ],
+            answer: 2,
+            image: "",
+        },
+        {}
+    ]
+
+    //question layout (html)
+
+    //game operaion functions
+})
